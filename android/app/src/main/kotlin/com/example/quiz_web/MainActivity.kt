6e6d407cc3c274e05315d833fb94d38c84fd35a3
Deepanshu Chowdhary <deepanshu.chowdhary@gmail.com>
2021-06-25 13:53:34 +0530

    Initial setup

---
+++ android/app/src/main/kotlin/com/example/quiz_web/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.quiz_web
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:quiz_web/presentation/screens/splashscreen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        ScreenUtil.init(
+          constraints,
+          designSize: const Size(1920, 1080),
+          allowFontScaling: true,
+        );
+        return MaterialApp(
+          title: 'Quizzy',
+          debugShowCheckedModeBanner: false,
+          home: SplashScreen(),
+        );
+      },
+    );
+  }
+}
